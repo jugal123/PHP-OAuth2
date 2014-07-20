c41dc1954bb40adc14ebc574121bf2e2e38e7ecf
jugal123 <jugal@alchemytechnologies.ca>
2014-07-20 17:36:13 +0530

    demo1.php
    hello

---
+++ src/OAuth2/demo1.php
@@ -0,0 +1,29 @@
+<?php
+require('client.php');
+require('GrantType/IGrantType.php');
+require('GrantType/AuthorizationCode.php');
+
+const CLIENT_ID     = 'your client id';
+const CLIENT_SECRET = 'your client secret';
+
+const REDIRECT_URI           = 'http://url/of/this.php';
+const AUTHORIZATION_ENDPOINT = 'https://alchemy1.nationbuilder.com/oauth/authorize';
+const TOKEN_ENDPOINT         = 'https://alchemy1.nationbuilder.com//oauth/access_token';
+
+$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
+if (!isset($_GET['code']))
+{
+    $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI);
+    header('Location: ' . $auth_url);
+    die('Redirect');
+}
+else
+{
+    $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
+    $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
+    parse_str($response['result'], $info);
+    $client->setAccessToken($info['access_token']);
+    $response = $client->fetch('https://alchemy1.nationbuilder.com/me');
+    var_dump($response, $response['result']);
+}
+?>
